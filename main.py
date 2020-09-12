25703128788b30c4b61ebaf08462fb8890b83be6
Hexadecimalz <Hexadecimalz@users.noreply.github.com>
2020-09-12 10:46:42 -0600

    Update main.py
    Update to Python 3.8.x

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 import time
-import urllib2
+from urllib.request import urlopen
 import os
 import sys
 '''
 Title: Bloomberg Terminal Update Check
-Python 2.7.x Linux / Windows / macOS
-Authors: A joint effort by two people that are not named
-
+Python 3.8.2 Linux / Windows / macOS
 This code will check the Bloomberg Software Website to see if an update has been released
-for an existing terminal installation.Updates can be released randomly, sometimes it's helpful
-to get ahead of the update, and Bloomberg releases links using a standard format -- though
-only allows auto-update through a throttled system using their application. People don't like slow.
-
-This code will cycle through all 31 days of all months, even months without 31 days. A file is
-created when the update is found, and the code will not produce a result again until the next
-month. You could program the script to e-mail you of pertinent changes very easily, or for a
-task to download the file to be distributed through the network.
+for an existing terminal installation. Updates can be released randomly, sometimes it's helpful
+to get ahead of the update, and Bloomberg releases links using a standard format. This code
+will cycle through all 31 days of all months, even months without 31 days. A file is created
+when the update is found, and the code will not produce a result again until the next month.
+You could program the script to e-mail you of pertinent changes very easily, or for a task
+to download the file to be distributed through the network.
 '''
 #Checks if a URL returns True or False
 def checkurl(url):
     try:
-        urllib2.urlopen(url)
+        urlopen(url)
         return True
     except:
         return False
+    
 #Check if a file is real
 def checkfile(file):
     try:
@@ -31,6 +28,7 @@ def checkfile(file):
             return True
     except:
         return False
+    
 #Computers are stupid, figure out where the hell I am
 def getScriptPath():
     return os.path.dirname(os.path.realpath(sys.argv[0]))
@@ -39,7 +37,7 @@ def main(argv):
     #Where the script is
     dir = getScriptPath()
     #Predefined URL bits from Bloomberg
-    urlstart = "http://bdn-ak.bloomberg.com/software/trv/bupd"
+    urlstart = "https://bdn-ak-ssl.bloomberg.com/software/trv/upgr"
     urlend = ".exe"
     #Below extracts the pertinent bits of date to get what we need
     date = (time.strftime("%m/%Y"))
@@ -54,6 +52,7 @@ def main(argv):
             day = str(x)
         fulldate = month + day + year
         fullurl = urlstart + fulldate + urlend
+        print (fullurl)
         if checkurl(fullurl) == True:
             if checkfile(dir+os.sep+fulldate) != True:
                 f= open(fulldate,"w+")
@@ -62,4 +61,4 @@ def main(argv):
 
 if __name__ == "__main__":
     if main(sys.argv) == True:
-        print "An update is found!"
+        print ("An update is found!")
